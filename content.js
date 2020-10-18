e9b795e5d66593906b15d9dd290d22758537e9d6
Benedek Zsombor Ákos <zsomborbeni@gmail.com>
2020-10-19 01:03:33 +0200

    mutation observer added for dynamically added videos

--- content.js
+++ content.js
@@ -1,24 +1,37 @@
 'use strict';
 
-if (window.contentScriptInjected !== true) {
-    window.contentScriptInjected = true;
+if (window.contentScriptVideo !== true) {
+    window.contentScriptVideo = true;
+
+    // Max allowed time offset between videos (in seconds)
+    const toffset = 0.5;
 
     // Init
-    let video = getVideo();
+    let video = document.querySelector('video');
 
     if (video) {
-        initSync();
+        video.addEventListener('pause', sendState);
+        video.addEventListener('play', sendState);
+        video.addEventListener('seeked', sendState);
+        sendState();
     }
 
-    function getVideo() {
-        let videos = document.getElementsByTagName('video');
-        if (videos.length > 0) {
-            return videos[0];
+    new MutationObserver(function (mutations, observer) {
+        for (const { addedNodes } of mutations) {
+            addedNodes.forEach((node) => {
+                if (node.nodeName === 'VIDEO') {
+                    video = node;
+                    video.addEventListener('pause', sendState);
+                    video.addEventListener('play', sendState);
+                    video.addEventListener('seeked', sendState);
+                    sendState();
+                }
+            });
         }
-    }
+    }).observe(document.body, { attributes: true, childList: true, subtree: true });
 
     function sendState() {
-        if (video.readyState > 2) {
+        if (video && video.readyState > 2) {
             var videoState = {
                 url: window.location.href,
                 isPaused: video.paused,
@@ -32,33 +45,20 @@ if (window.contentScriptInjected !== true) {
         }
     }
 
-    function initSync() {
-        // Max allowed time offset between videos (in seconds)
-        const toffset = 0.5;
-
-        // Sending messages if user clicks
-        sendState();
-
-        video.addEventListener('pause', sendState);
-        video.addEventListener('play', sendState);
-        video.addEventListener('seeked', sendState);
-
-        // Gets video's state from storage
-        chrome.storage.onChanged.addListener(function (changes, namespace) {
-            for (var key in changes) {
-                if (key == 'videoState') {
-                    let videoState = changes[key].newValue;
-                    let url = window.location.href;
-                    if (url.startsWith(videoState.url) || videoState.url.startsWith(url)) {
-                        if (video.paused !== videoState.isPaused)
-                            videoState.isPaused ? video.pause() : video.play();
-                        let timediff = Math.abs(video.currentTime - videoState.currentTime).toFixed(1);
-                        if (timediff > toffset && video.readyState > 2) {
-                            video.currentTime = videoState.currentTime;
-                        }
+    chrome.storage.onChanged.addListener(function (changes, namespace) {
+        for (var key in changes) {
+            if (video && key == 'videoState') {
+                let videoState = changes[key].newValue;
+                let url = window.location.href;
+                if (url.startsWith(videoState.url) || videoState.url.startsWith(url)) {
+                    if (video.paused !== videoState.isPaused)
+                        videoState.isPaused ? video.pause() : video.play();
+                    let timediff = Math.abs(video.currentTime - videoState.currentTime).toFixed(1);
+                    if (timediff > toffset && video.readyState > 2) {
+                        video.currentTime = videoState.currentTime;
                     }
                 }
             }
-        });
-    }
+        }
+    });
 }
